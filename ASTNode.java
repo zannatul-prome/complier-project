f26fd8b1b59463111adb95f61fe254ad02848446
Marufa shipa <marufashipa@gmail.com>
2026-04-29 17:44:05 +0600

    Initial commit: code change

--- ASTNode.java
+++ ASTNode.java
@@ -1,14 +1,17 @@
 public class ASTNode {
+    
     String type;
     String value;
     ASTNode left, right;
     
     public ASTNode(String type, String value) {
+
         this.type = type;
         this.value = value;
     }
     
     public ASTNode(String type, ASTNode left, ASTNode right) {
+
         this.type = type;
         this.left = left;
         this.right = right;
--- Parser.java
+++ Parser.java
@@ -224,7 +224,7 @@ public class Parser {
     void parseStatement() {
 
 
-        
+
         Token varToken = current();
 
         if (varToken.type == TokenType.IDENTIFIER) {
