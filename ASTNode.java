b111b0282c2b2054a1ed88ed14e70edf5754a652
Marufa shipa <marufashipa@gmail.com>
2026-04-29 18:49:40 +0600

    improved ASTNode structure and readability

--- ASTNode.java
+++ ASTNode.java
@@ -1,19 +1,22 @@
 public class ASTNode {
-    
+
     String type;
     String value;
-    ASTNode left, right;
-    
-    public ASTNode(String type, String value) {
+    ASTNode left;
+    ASTNode right;
 
+    // Leaf node 
+    public ASTNode(String type, String value) {
         this.type = type;
         this.value = value;
+        this.left = null;
+        this.right = null;
     }
-    
-    public ASTNode(String type, ASTNode left, ASTNode right) {
 
+
+    public ASTNode(String type, ASTNode left, ASTNode right) {
         this.type = type;
-        
+        this.value = null; // internal node usually value na
         this.left = left;
         this.right = right;
     }
