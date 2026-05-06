e150aa0d284d50205fbbb7e4c7b7ecb835c903c3
Adiat Chowdhury <you@example.com>
2026-05-06 22:19:44 +0600

    my local changes

--- ASTNode.java
+++ ASTNode.java
@@ -1,13 +1,15 @@
+
+// AST Node class for syntax tree
 public class ASTNode {
     String type;
     String value;
     ASTNode left, right;
-    
+
     public ASTNode(String type, String value) {
         this.type = type;
         this.value = value;
     }
-    
+
     public ASTNode(String type, ASTNode left, ASTNode right) {
         this.type = type;
         this.left = left;
